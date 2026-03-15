ef9e4acc4fbbdba68d45af12cf8919f021d87dcf
Kush Bhogate <kushbg1610@gmail.com>
2026-03-15 16:12:18 +0530

    model and metadata saving

--- src/baseline_model.py
+++ src/baseline_model.py
@@ -1,8 +1,19 @@
-from utils.dataloader import load_data
-from utils.traintestsplit import train_val_test_split
+import sys 
+import os 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.utils.dataloader import load_data
+from src.utils.traintestsplit import train_val_test_split
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_recall_curve, roc_auc_score,f1_score,confusion_matrix,average_precision_score
+import joblib
+
+import json
+
+BASE_DIR = os.path.dirname(__file__)
+PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, ".."))
+MODELS_DIR = os.path.join(PROJECT_ROOT, "models")
+os.makedirs(MODELS_DIR, exist_ok=True)
 
 X, y = load_data(r"D:\Kush\2nd Year\projects\MLOPs\data\creditcard_scaled.csv")
 X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(X, y)
@@ -22,3 +33,23 @@ print("Confusion matrix:\n", confusion_matrix(y_val, predictions))
 print("Precision-Recall curve:\n", average_precision_score(y_val, probs))
 
 
+
+joblib.dump(model, os.path.join(MODELS_DIR, "baseline_model.joblib"))
+
+
+
+metadata = {
+    "model_name": "Logistic Regression",
+    "hyperparameters": {
+        "max_iter": 1000,
+        "class_weight": "balanced",
+        "random_state": 42
+    },
+    "validation_metrics": {
+        "roc_auc": roc_score,
+        "f1_score": f1
+    }
+}
+
+with open(os.path.join(MODELS_DIR, "baseline_model_metadata.json"), "w") as f:
+    json.dump(metadata, f, indent=4)
